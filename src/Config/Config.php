b3a25c03ef9ce60061342a6b68350b5a3a0917a7
Роман Собороветс <79000514088ro@gmail.com>
2025-04-16 23:04:29 +0700

    Улучшение визуализации сайта

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,7 +2,6 @@
 namespace App\Config;
 class Config{
 const FILE_PRODUCTS=".\Storage\data.json";
-<<<<<<< HEAD
 const FILE_ORDERS=".\storage\order.json";
 const TYPE_FILE="file";
 const TYPE_DB="db";
@@ -16,6 +15,4 @@ const TABLE_ORDERS="orders";
 
 // Режим хранения данных 
 const STORAGE_TYPE= self::TYPE_DB;
-=======
->>>>>>> 81eb876697f261033b16b1a38438cc72dbad5f88
 }
